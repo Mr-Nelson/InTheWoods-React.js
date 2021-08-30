0a4fd0d760b68c8825366a03a7f4e03697669690
Mr-Nelson <kdnels88@gmail.com>
2021-08-30 11:41:58 -0500

    created forms for both documents and departments

--- src/components/Departments/department.jsx
+++ src/components/Departments/department.jsx
@@ -3,30 +3,90 @@ import {useState} from "react";
 import useForm from '../UseForm/useForm';
 import axios from 'axios';
 
-
 const Department = (props) => {
-    const [show, setShow] = useState(false);  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);   
-    const{values, handleChange, handleSubmit} = useForm(documentDetails);
-    function documentDetails() {
-        getDocumentation();
-    } 
-
-    const getDocumentation = async (event) => {
-        const jwt =localStorage.getItem("token");
-        const res = await axios.post(`https://localhost:44394/api/department`,
-    event, {headers: { Authorization: "Bearer " + jwt }
-        })
-        .then(res => {
-                var documents = res.data
-        })
-        .catch(err => console.log(err))    
-        };
-
+    const { values, handleChange, handleSubmit } = useForm(create);
+    const [ redirect, setRedirect] = useState(false);
+    function create() {
+      props.postDepartment(values);
+      setRedirect(true);
+    }
+  
     return (
-        <h2> Here are the departments.</h2>
-    )
-}
-
-export default Department;
+      <div>
+        <div>
+          <div className="row row-spacer">
+            <h1>Departments</h1>
+          </div>
+  
+          <form className="col-md-2" onSubmit={handleSubmit}>
+            <h1 className="h3 mb-3 fw-normal">Please Register</h1>
+  
+            <div className="form-floating">
+              <input
+                name="Company"
+                type="string"
+                className="form-control"
+                placeholder="Inwood City Hall"
+                required
+                onChange={handleChange}
+                values={values.company}
+              />
+              <label for="floatingInput">Company Name </label>
+            </div>
+            <div className="form-floating">
+              <input
+                name="Address"
+                type="string"
+                className="form-control"
+                placeholder="123 Main St, Inwood, IA 51240"
+                onChange={handleChange}
+                values={values.address}
+              />
+              <label for="floatingPassword">Address</label>
+            </div>
+            <div className="form-floating">
+              <input
+                name="Hours"
+                type="string"
+                className="form-control"
+                placeholder="9AM - 4PM"
+                onChange={handleChange}
+                values={values.hours}
+              />
+              <label for="floatingInput">Hours</label>
+            </div>
+            <div className="form-floating">
+              <input
+                name="ManagerName"
+                type="string"
+                className="form-control"
+                placeholder="City Clerk: Jane Doe"
+                onChange={handleChange}
+                values={values.managername}
+              />
+              <label for="floatingInput">Manager Name</label>
+            </div>
+            <div className="form-floating">
+              <input
+                name="PhoneNumber"
+                type="phoneNumber"
+                className="form-control"
+                placeholder="555-555-5555"
+                onChange={handleChange}
+                values={values.phonenumber}
+              />
+              <label for="floatingInput">Phone Number</label>
+            </div>
+            <div className="form-floating">
+            <button className="w-10 btn btn-lg btn-primary" type="submit">
+              REGISTER
+            </button>
+            </div>
+            <p className="mt-5 mb-3 text-muted">© 2021</p>
+          </form>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Department;
--- src/components/Documents/documents.jsx
+++ src/components/Documents/documents.jsx
@@ -5,28 +5,56 @@ import axios from 'axios';
 
 
 const Document = (props) => {
-    const [show, setShow] = useState(false);  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);   
-    const{values, handleChange, handleSubmit} = useForm(documentDetails);
-    function documentDetails() {
-        getDocumentation();
-    } 
-
-    const getDocumentation = async (event) => {
-        const jwt =localStorage.getItem("token");
-        const res = await axios.post(`https://localhost:44394/api/document`,
-    event, {headers: { Authorization: "Bearer " + jwt }
-        })
-        .then(res => {
-                var documents = res.data
-        })
-        .catch(err => console.log(err))    
-        };
-
+    const { values, handleChange, handleSubmit } = useForm(create);
+    const [ redirect, setRedirect] = useState(false);
+    function create() {
+      props.postDocument(values);
+      setRedirect(true);
+    }
+    
     return (
-        <h2> Here are the documents.</h2>
-    )
-}
-
-export default Document;
+      <div>
+        <div>
+          <div className="row row-spacer">
+            <h1>Inwood Documents</h1>
+          </div>
+  
+          <form className="col-md-2" onSubmit={handleSubmit}>
+            <h1 className="h3 mb-3 fw-normal">Register Document</h1>
+  
+            <div className="form-floating">
+              <input
+                name="DocumentName"
+                type="string"
+                className="form-control"
+                placeholder="By Laws"
+                required
+                onChange={handleChange}
+                values={values.documentname}
+              />
+              <label for="floatingInput">Document Name </label>
+            </div>
+            <div className="form-floating">
+              <input
+                name="DocumentDescription"
+                type="string"
+                className="form-control"
+                placeholder="This is a letter."
+                onChange={handleChange}
+                values={values.documentdescription}
+              />
+              <label for="floatingPassword">Document Description</label>
+            </div>
+            <div className="form-floating">
+            <button className="w-10 btn btn-lg btn-primary" type="submit">
+              REGISTER
+            </button>
+            </div>
+            <p className="mt-5 mb-3 text-muted">© 2021</p>
+          </form>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Document;
