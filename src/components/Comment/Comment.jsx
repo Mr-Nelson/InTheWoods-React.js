e073fba9006b1a86f81a696739e0e0bd43593bc1
Mr-Nelson <kdnels88@gmail.com>
2021-08-30 13:39:50 -0500

    more CSS styling for comments/subcomments

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -7,13 +7,12 @@ const Comment = (props) => {
     const {values, handleChange, handleSubmit} = useForm(create);
     function create () {
         props.postComment(values);
-        props.postSubComment(values);
     }
 
     return (
         <React.Fragment>
-            <div class="column" width="4"></div>
-                <div class="timeline-item" width="6">
+            <div class="column" width="100%"></div>
+                <div class="d-flex justify-content-center" width="max-width">
                     <tbody>
                         <div class="container">
                             <tr><td>Into The Woods Q and A</td></tr>
@@ -32,7 +31,7 @@ const Comment = (props) => {
                                     />
                                     <label for="floatingInput">User Comment </label>
                                 </div>
-                                <div className="form-floating">
+                                <div className="form-floating" align="right">
                                     <button className="w-10 btn btn-lg btn-primary" type="submit">
                                     Submit
                                     </button>
@@ -43,53 +42,59 @@ const Comment = (props) => {
                         <div class="container">
                             {props.comments.map((comment, id) => {
                                 return (
-                                    <tr key= {id}>
-                                        <td>{comment.userComment}</td>
-                                        <td className="d-flex justify-content-center">
-                                            <button className="btn btn-primary w-md-25" onClick={() => props.getAllSubComments(comment.id)}>SubComments</button>
-                                        </td>
-                                        <td>
-                                            {props.subcomments.map((subcomment, id) => {
-                                                return (
-                                                    <tr key= {id}>
-                                                        <td>{subcomment.userSubComment}</td>
-                                                    </tr>
-                                                )
-                                            })
-                                            }
-                                        </td>
-                                    </tr>
-                                )
+                                    <tr className="table-row" key= {id}>
+                                        <span>
+                                            <td>{comment.userComment}</td>
+                                        </span>
+                                        <span>
+                                            <td className="d-flex justify-content-center" align="bottom">
+                                                <button className="btn btn-primary w-md-25" onClick={() => props.getAllSubComments(comment.id)}>SubComments</button>
+                                            </td>
+                                        </span>
+                                        <span>
+                                            <td>
+                                                {props.subcomments.map((subcomment, id) => {
+                                                    return (
+                                                        <tr key= {id}>
+                                                            <span><td>{subcomment.userSubComment}</td></span>
+                                                        </tr>
+                                                    )
+                                                })
+                                                }
+                                            </td>
+                                        </span>
+                                        <span>
+                                            <td className="d-flex justify-content-center">
+                                                <form className="col-md-25" onSubmit={handleSubmit}>
+                                                    <h4 className="h3 mb-3 fw-normal">Leave a SubComment!</h4>
+                    
+                                                    <div className="form-floating">
+                                                        <input
+                                                            name="UserSubComment"
+                                                            type="string"
+                                                            className="form-control"
+                                                            placeholder="CommentHere"
+                                                            required
+                                                            onChange={handleChange}
+                                                            values={values.userSubComment}
+                                                        />
+                                                        <label for="floatingInput">User SubComment </label>
+                                                    </div>
+                                                    <div className="form-floating" align="right">
+                                                        <button className="w-10 btn btn-lg btn-primary" type="submit">
+                                                        Submit
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </td>
+                                        </span>
+                                    </tr>    
+                                )                   
                             })}  
-                        </div>
-                        <div class="container">                  
-                            <td className="d-flex justify-content-center">
-                            <form className="col-md-25" onSubmit={handleSubmit}>
-                                <h4 className="h3 mb-3 fw-normal">Leave a SubComment!</h4>
-
-                                <div className="form-floating">
-                                    <input
-                                        name="UserSubComment"
-                                        type="string"
-                                        className="form-control"
-                                        placeholder="CommentHere"
-                                        required
-                                        onChange={handleChange}
-                                        values={values.userSubComment}
-                                    />
-                                    <label for="floatingInput">User SubComment </label>
-                                </div>
-                                <div className="form-floating">
-                                    <button className="w-10 btn btn-lg btn-primary" type="submit">
-                                    Submit
-                                    </button>
-                                </div>
-                            </form>
-                            </td>
                         </div>
                 </tbody>
             </div>
-        <div class="column" width="4"></div>         
+        <div class="column" width="100%"></div>         
         <p className="mt-5 mb-3 text-muted">© 2021</p> 
         </React.Fragment>
     )
